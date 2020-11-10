ca2c7046e8fd0967dc22c9fe3b04e11a42315a9b
Gökhan ZER <martperisi@gmail.com>
2020-11-11 01:51:19 +0300

    spa

---
+++ Source/cppTest13/MyMyTriggerBoxSpecific.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyTriggerBox.h"
+#include "MyMyTriggerBoxSpecific.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPTEST13_API AMyMyTriggerBoxSpecific : public AMyTriggerBox
+{
+	GENERATED_BODY()
+	
+};
