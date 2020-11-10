4b823bec6934d44978b2941e4cd08fc752cea3ae
Gökhan ZER <martperisi@gmail.com>
2020-11-10 10:55:32 +0300

    adet çarpanı ön yüze eklendi

---
+++ Source/cppTest13/MyTriggerBoxActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TriggerBoxActor.h"
+#include "MyTriggerBoxActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPTEST13_API AMyTriggerBoxActor : public ATriggerBoxActor
+{
+	GENERATED_BODY()
+	
+};
