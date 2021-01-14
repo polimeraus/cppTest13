d6cb007dc17390bc36bae998e73e61b31fc5e31e
Gökhan ZER <martperisi@gmail.com>
2021-01-15 00:31:02 +0300

    aaa

---
+++ Source/cppTest13/MyMeshComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/MeshComponent.h"
+#include "MyMeshComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPTEST13_API UMyMeshComponent : public UMeshComponent
+{
+	GENERATED_BODY()
+	
+};
